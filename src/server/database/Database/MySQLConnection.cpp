allankrdec/TrinityCore-10-2024

Core/DBLayer: Support using mysql 8

src/server/database/Database/MySQLConnection.cpp
@@ -19,16 +19,15 @@
 #include "Common.h"
 #include "DatabaseWorker.h"
 #include "Log.h"
+#include "MySQLHacks.h"
+#include "MySQLPreparedStatement.h"
 #include "PreparedStatement.h"
 #include "QueryResult.h"
 #include "Timer.h"
 #include "Transaction.h"
 #include "Util.h"
-#ifdef _WIN32 // hack for broken mysql.h not including the correct winsock header for SOCKET definition, fixed in 5.7
-#include <winsock2.h>
-#endif
 #include <errmsg.h>
-#include <mysql.h>
+#include "MySQLWorkaround.h"
 #include <mysqld_error.h>
 
 MySQLConnectionInfo::MySQLConnectionInfo(std::string const& infoString)
@@ -130,8 +129,8 @@ uint32 MySQLConnection::Open()
     }
     #endif
 
-    m_Mysql = mysql_real_connect(mysqlInit, m_connectionInfo.host.c_str(), m_connectionInfo.user.c_str(),
-        m_connectionInfo.password.c_str(), m_connectionInfo.database.c_str(), port, unix_socket, 0);
+    m_Mysql = reinterpret_cast<MySQLHandle*>(mysql_real_connect(mysqlInit, m_connectionInfo.host.c_str(), m_connectionInfo.user.c_str(),
+        m_connectionInfo.password.c_str(), m_connectionInfo.database.c_str(), port, unix_socket, 0));
 
     if (m_Mysql)
     {
@@ -241,7 +240,7 @@ bool MySQLConnection::Execute(PreparedStatementBase* stmt)
     return true;
 }
 
-bool MySQLConnection::_Query(PreparedStatementBase* stmt, MYSQL_RES** pResult, uint64* pRowCount, uint32* pFieldCount)
+bool MySQLConnection::_Query(PreparedStatementBase* stmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount)
 {
     if (!m_Mysql)
         return false;
@@ -288,7 +287,7 @@ bool MySQLConnection::_Query(PreparedStatementBase* stmt, MYSQL_RES** pResult, u
 
     m_mStmt->ClearParameters();
 
-    *pResult = mysql_stmt_result_metadata(msql_STMT);
+    *pResult = reinterpret_cast<MySQLResult*>(mysql_stmt_result_metadata(msql_STMT));
     *pRowCount = mysql_stmt_num_rows(msql_STMT);
     *pFieldCount = mysql_stmt_field_count(msql_STMT);
 
@@ -300,8 +299,8 @@ ResultSet* MySQLConnection::Query(const char* sql)
     if (!sql)
         return NULL;
 
-    MYSQL_RES *result = NULL;
-    MYSQL_FIELD *fields = NULL;
+    MySQLResult* result = NULL;
+    MySQLField* fields = NULL;
     uint64 rowCount = 0;
     uint32 fieldCount = 0;
 
@@ -311,7 +310,7 @@ ResultSet* MySQLConnection::Query(const char* sql)
     return new ResultSet(result, fields, rowCount, fieldCount);
 }
 
-bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD **pFields, uint64* pRowCount, uint32* pFieldCount)
+bool MySQLConnection::_Query(const char* sql, MySQLResult** pResult, MySQLField** pFields, uint64* pRowCount, uint32* pFieldCount)
 {
     if (!m_Mysql)
         return false;
@@ -333,7 +332,7 @@ bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD *
         else
             TC_LOG_DEBUG("sql.sql", "[%u ms] SQL: %s", getMSTimeDiff(_s, getMSTime()), sql);
 
-        *pResult = mysql_store_result(m_Mysql);
+        *pResult = reinterpret_cast<MySQLResult*>(mysql_store_result(m_Mysql));
         *pRowCount = mysql_affected_rows(m_Mysql);
         *pFieldCount = mysql_field_count(m_Mysql);
     }
@@ -347,7 +346,7 @@ bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD *
         return false;
     }
 
-    *pFields = mysql_fetch_fields(*pResult);
+    *pFields = reinterpret_cast<MySQLField*>(mysql_fetch_fields(*pResult));
 
     return true;
 }
@@ -418,6 +417,11 @@ int MySQLConnection::ExecuteTransaction(std::shared_ptr<TransactionBase> transac
     return 0;
 }
 
+size_t MySQLConnection::EscapeString(char* to, const char* from, size_t length)
+{
+    return mysql_real_escape_string(m_Mysql, to, from, length);
+}
+
 void MySQLConnection::Ping()
 {
     mysql_ping(m_Mysql);
@@ -436,6 +440,11 @@ bool MySQLConnection::LockIfReady()
 void MySQLConnection::Unlock()
 {
     m_Mutex.unlock();
+}
+
+uint32 MySQLConnection::GetServerVersion() const
+{
+    return mysql_get_server_version(m_Mysql);
 }
 
 MySQLPreparedStatement* MySQLConnection::GetPreparedStatement(uint32 index)
@@ -477,13 +486,13 @@ void MySQLConnection::PrepareStatement(uint32 index, std::string const& sql, Con
             m_prepareError = true;
         }
         else
-            m_stmts[index] = Trinity::make_unique<MySQLPreparedStatement>(stmt, sql);
+            m_stmts[index] = Trinity::make_unique<MySQLPreparedStatement>(reinterpret_cast<MySQLStmt*>(stmt), sql);
     }
 }
 
 PreparedResultSet* MySQLConnection::Query(PreparedStatementBase* stmt)
 {
-    MYSQL_RES *result = NULL;
+    MySQLResult* result = NULL;
     uint64 rowCount = 0;
     uint32 fieldCount = 0;
 
@@ -509,7 +518,7 @@ bool MySQLConnection::_HandleMySQLErrno(uint32 errNo, uint8 attempts /*= 5*/)
             {
                 TC_LOG_ERROR("sql.sql", "Lost the connection to the MySQL server!");
 
-                mysql_close(GetHandle());
+                mysql_close(m_Mysql);
                 m_Mysql = nullptr;
             }
 

src/server/database/Database/MySQLPreparedStatement.h
@@ -1,0 +1,77 @@
+/*
+ * Copyright (C) 2008-2019 TrinityCore <https://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MySQLPreparedStatement_h__
+#define MySQLPreparedStatement_h__
+
+#include "DatabaseEnvFwd.h"
+#include "Define.h"
+#include "MySQLWorkaround.h"
+#include <string>
+#include <vector>
+
+class MySQLConnection;
+class PreparedStatementBase;
+
+//- Class of which the instances are unique per MySQLConnection
+//- access to these class objects is only done when a prepared statement task
+//- is executed.
+class TC_DATABASE_API MySQLPreparedStatement
+{
+    friend class MySQLConnection;
+    friend class PreparedStatementBase;
+
+    public:
+        MySQLPreparedStatement(MySQLStmt* stmt, std::string queryString);
+        ~MySQLPreparedStatement();
+
+        void setNull(const uint8 index);
+        void setBool(const uint8 index, const bool value);
+        void setUInt8(const uint8 index, const uint8 value);
+        void setUInt16(const uint8 index, const uint16 value);
+        void setUInt32(const uint8 index, const uint32 value);
+        void setUInt64(const uint8 index, const uint64 value);
+        void setInt8(const uint8 index, const int8 value);
+        void setInt16(const uint8 index, const int16 value);
+        void setInt32(const uint8 index, const int32 value);
+        void setInt64(const uint8 index, const int64 value);
+        void setFloat(const uint8 index, const float value);
+        void setDouble(const uint8 index, const double value);
+        void setBinary(const uint8 index, const std::vector<uint8>& value, bool isString);
+
+        uint32 GetParameterCount() const { return m_paramCount; }
+
+    protected:
+        MySQLStmt* GetSTMT() { return m_Mstmt; }
+        MySQLBind* GetBind() { return m_bind; }
+        PreparedStatementBase* m_stmt;
+        void ClearParameters();
+        void AssertValidIndex(uint8 index);
+        std::string getQueryString() const;
+
+    private:
+        MySQLStmt* m_Mstmt;
+        uint32 m_paramCount;
+        std::vector<bool> m_paramsSet;
+        MySQLBind* m_bind;
+        std::string const m_queryString;
+
+        MySQLPreparedStatement(MySQLPreparedStatement const& right) = delete;
+        MySQLPreparedStatement& operator=(MySQLPreparedStatement const& right) = delete;
+};
+
+#endif // MySQLPreparedStatement_h__

nbyaya/The-Cataclysm-Preservation-Project

src/server/database/Database/MySQLConnection.cpp
@@ -20,16 +20,15 @@
 #include "Common.h"
 #include "DatabaseWorker.h"
 #include "Log.h"
+#include "MySQLHacks.h"
+#include "MySQLPreparedStatement.h"
 #include "PreparedStatement.h"
 #include "QueryResult.h"
 #include "Timer.h"
 #include "Transaction.h"
 #include "Util.h"
 #include <errmsg.h>
-#ifdef _WIN32 // hack for broken mysql.h not including the correct winsock header for SOCKET definition, fixed in 5.7
-#include <winsock2.h>
-#endif
-#include <mysql.h>
+#include "MySQLWorkaround.h"
 #include <mysqld_error.h>
 
 MySQLConnectionInfo::MySQLConnectionInfo(std::string const& infoString)
@@ -129,8 +128,8 @@ uint32 MySQLConnection::Open()
     }
     #endif
 
-    m_Mysql = mysql_real_connect(mysqlInit, m_connectionInfo.host.c_str(), m_connectionInfo.user.c_str(),
-        m_connectionInfo.password.c_str(), m_connectionInfo.database.c_str(), port, unix_socket, 0);
+    m_Mysql = reinterpret_cast<MySQLHandle*>(mysql_real_connect(mysqlInit, m_connectionInfo.host.c_str(), m_connectionInfo.user.c_str(),
+        m_connectionInfo.password.c_str(), m_connectionInfo.database.c_str(), port, unix_socket, 0));
 
     if (m_Mysql)
     {
@@ -240,7 +239,7 @@ bool MySQLConnection::Execute(PreparedStatement* stmt)
     return true;
 }
 
-bool MySQLConnection::_Query(PreparedStatement* stmt, MYSQL_RES **pResult, uint64* pRowCount, uint32* pFieldCount)
+bool MySQLConnection::_Query(PreparedStatement* stmt, MySQLResult**pResult, uint64* pRowCount, uint32* pFieldCount)
 {
     if (!m_Mysql)
         return false;
@@ -287,7 +286,7 @@ bool MySQLConnection::_Query(PreparedStatement* stmt, MYSQL_RES **pResult, uint6
 
     m_mStmt->ClearParameters();
 
-    *pResult = mysql_stmt_result_metadata(msql_STMT);
+    *pResult = reinterpret_cast<MySQLResult*>(mysql_stmt_result_metadata(msql_STMT));
     *pRowCount = mysql_stmt_num_rows(msql_STMT);
     *pFieldCount = mysql_stmt_field_count(msql_STMT);
 
@@ -299,8 +298,8 @@ ResultSet* MySQLConnection::Query(char const* sql)
     if (!sql)
         return nullptr;
 
-    MYSQL_RES *result = nullptr;
-    MYSQL_FIELD *fields = nullptr;
+    MySQLResult* result = nullptr;
+    MySQLField* fields = nullptr;
     uint64 rowCount = 0;
     uint32 fieldCount = 0;
 
@@ -310,7 +309,7 @@ ResultSet* MySQLConnection::Query(char const* sql)
     return new ResultSet(result, fields, rowCount, fieldCount);
 }
 
-bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD **pFields, uint64* pRowCount, uint32* pFieldCount)
+bool MySQLConnection::_Query(const char *sql, MySQLResult**pResult, MySQLField**pFields, uint64* pRowCount, uint32* pFieldCount)
 {
     if (!m_Mysql)
         return false;
@@ -332,7 +331,7 @@ bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD *
         else
             TC_LOG_DEBUG("sql.sql", "[%u ms] SQL: %s", getMSTimeDiff(_s, getMSTime()), sql);
 
-        *pResult = mysql_store_result(m_Mysql);
+        *pResult = reinterpret_cast<MySQLResult*>(mysql_store_result(m_Mysql));
         *pRowCount = mysql_affected_rows(m_Mysql);
         *pFieldCount = mysql_field_count(m_Mysql);
     }
@@ -346,7 +345,7 @@ bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD *
         return false;
     }
 
-    *pFields = mysql_fetch_fields(*pResult);
+    *pFields = reinterpret_cast<MySQLField*>(mysql_fetch_fields(*pResult));
 
     return true;
 }
@@ -417,6 +416,11 @@ int MySQLConnection::ExecuteTransaction(SQLTransaction& transaction)
     return 0;
 }
 
+size_t MySQLConnection::EscapeString(char* to, const char* from, size_t length)
+{
+    return mysql_real_escape_string(m_Mysql, to, from, length);
+}
+
 void MySQLConnection::Ping()
 {
     mysql_ping(m_Mysql);
@@ -436,6 +440,12 @@ void MySQLConnection::Unlock()
 {
     m_Mutex.unlock();
 }
+
+uint32 MySQLConnection::GetServerVersion() const
+{
+    return mysql_get_server_version(m_Mysql);
+}
+
 
 MySQLPreparedStatement* MySQLConnection::GetPreparedStatement(uint32 index)
 {
@@ -476,13 +486,13 @@ void MySQLConnection::PrepareStatement(uint32 index, std::string const& sql, Con
             m_prepareError = true;
         }
         else
-            m_stmts[index] = Trinity::make_unique<MySQLPreparedStatement>(stmt, sql);
+            m_stmts[index] = Trinity::make_unique<MySQLPreparedStatement>(reinterpret_cast<MySQLStmt*>(stmt), sql);
     }
 }
 
 PreparedResultSet* MySQLConnection::Query(PreparedStatement* stmt)
 {
-    MYSQL_RES *result = nullptr;
+    MySQLResult* result = nullptr;
     uint64 rowCount = 0;
     uint32 fieldCount = 0;
 
@@ -509,7 +519,7 @@ bool MySQLConnection::_HandleMySQLErrno(uint32 errNo, uint8 attempts /*= 5*/)
             {
                 TC_LOG_ERROR("sql.sql", "Lost the connection to the MySQL server!");
 
-                mysql_close(GetHandle());
+                mysql_close(m_Mysql);
                 m_Mysql = nullptr;
             }
         }

src/server/database/Database/MySQLPreparedStatement.h
@@ -1,0 +1,77 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MySQLPreparedStatement_h__
+#define MySQLPreparedStatement_h__
+
+#include "DatabaseEnvFwd.h"
+#include "Define.h"
+#include "MySQLWorkaround.h"
+#include <string>
+#include <vector>
+
+class MySQLConnection;
+class PreparedStatement;
+
+//- Class of which the instances are unique per MySQLConnection
+//- access to these class objects is only done when a prepared statement task
+//- is executed.
+class TC_DATABASE_API MySQLPreparedStatement
+{
+    friend class MySQLConnection;
+    friend class PreparedStatementBase;
+
+    public:
+        MySQLPreparedStatement(MySQLStmt* stmt, std::string queryString);
+        ~MySQLPreparedStatement();
+
+        void setNull(const uint8 index);
+        void setBool(const uint8 index, const bool value);
+        void setUInt8(const uint8 index, const uint8 value);
+        void setUInt16(const uint8 index, const uint16 value);
+        void setUInt32(const uint8 index, const uint32 value);
+        void setUInt64(const uint8 index, const uint64 value);
+        void setInt8(const uint8 index, const int8 value);
+        void setInt16(const uint8 index, const int16 value);
+        void setInt32(const uint8 index, const int32 value);
+        void setInt64(const uint8 index, const int64 value);
+        void setFloat(const uint8 index, const float value);
+        void setDouble(const uint8 index, const double value);
+        void setBinary(const uint8 index, const std::vector<uint8>& value, bool isString);
+
+        uint32 GetParameterCount() const { return m_paramCount; }
+
+    protected:
+        MySQLStmt* GetSTMT() { return m_Mstmt; }
+        MySQLBind* GetBind() { return m_bind; }
+        PreparedStatement* m_stmt;
+        void ClearParameters();
+        void AssertValidIndex(uint8 index);
+        std::string getQueryString() const;
+
+    private:
+        MySQLStmt* m_Mstmt;
+        uint32 m_paramCount;
+        std::vector<bool> m_paramsSet;
+        MySQLBind* m_bind;
+        std::string const m_queryString;
+
+        MySQLPreparedStatement(MySQLPreparedStatement const& right) = delete;
+        MySQLPreparedStatement& operator=(MySQLPreparedStatement const& right) = delete;
+};
+
+#endif // MySQLPreparedStatement_h__